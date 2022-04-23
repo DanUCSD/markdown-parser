0ed72107df741922b4e149a12168cccc9dea3671
salbybba <amblas@ucsd.edu>
2022-04-22 19:42:12 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,13 +12,37 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws Exception {
+    public void testGetLinksFile() throws Exception {
         Path fileName = Path.of(("test-file.md"));
         String content = Files.readString(fileName);
         String[] str = {"https://something.com", "some-thing.html"};
         ArrayList<String> arr = new ArrayList<>(Arrays.asList(str));
-
         assertEquals(MarkdownParse.getLinks(content), arr);
 
+    }
+
+    @Test
+    public void testGetLinksBrackets() throws Exception {
+        Path fileName = Path.of(("test-brackets.md"));
+        String content = Files.readString(fileName);
+        ArrayList<String> arr = new ArrayList<>();
+        assertEquals(MarkdownParse.getLinks(content), arr);
+    }
+
+    @Test
+    public void testGetLinksBreak() throws Exception {
+        Path fileName = Path.of(("test-break.md"));
+        String content = Files.readString(fileName);
+        String[] str = {"https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa752574(v=vs.85)?redirectedfrom=MSDN"};
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(str));
+        assertEquals(MarkdownParse.getLinks(content), arr);
+    }
+
+    @Test
+    public void testGetLinksEmpty() throws Exception {
+        Path fileName = Path.of(("test-empty.md"));
+        String content = Files.readString(fileName);
+        ArrayList<String> arr = new ArrayList<>();
+        assertEquals(MarkdownParse.getLinks(content), arr);
     }
 }
