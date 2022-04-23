bf1f01b39bc0cd19a906c2f03de823a2682453aa
salbybba <amblas@ucsd.edu>
2022-04-22 19:26:57 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws Exception {
+        Path fileName = Path.of(("test-file.md"));
+        String content = Files.readString(fileName);
+        String[] str = {"https://something.com", "some-page.html"};
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(str));
+        assertEquals(MarkdownParse.getLinks(content), arr);
     }
 }
